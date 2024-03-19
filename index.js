4a6b6733bf6d50be371674ecefd5d35ea548e646
azumahjoshua <joshuaazumah1086@gmail.com>
2024-03-19 00:01:28 +0000

    user model added

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express")
 const app = express();
+const dotenv = require('dotenv')
+const mongoose = require("mongoose")
 
+dotenv.config();
+
+// connect to DB
+mongoose.connect(process.env.DB_CONNECT)
 const authRouth = require("./routes/auth")
+
 
 app.use("/api/user",authRouth)
 app.listen(5000,()=>console.log("Server Up and running"))
+
+// GDNjrR2QcoAZvhM9
