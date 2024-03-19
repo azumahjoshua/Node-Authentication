76a83747b475587a808f4fe3c47cbc8754565928
azumahjoshua <joshuaazumah1086@gmail.com>
2024-03-19 07:58:46 +0000

    error validation

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require("express")
 const app = express();
 const dotenv = require('dotenv')
 const mongoose = require("mongoose")
+const authRouth = require("./routes/auth")
 
 dotenv.config();
 
 // connect to DB
 mongoose.connect(process.env.DB_CONNECT)
-const authRouth = require("./routes/auth")
 
+app.use(express.json())
 
 app.use("/api/user",authRouth)
 app.listen(5000,()=>console.log("Server Up and running"))
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,42 @@
-const router = require("express").Router()
+const router = require("express").Router();
+const User = require("../model/User");
+const { registerValidation,loginValidation} = require('../validation')
+// Validation 
+const joi = require("joi");
 
-router.post('/register',(req,res)=>{
-    res.send("Register")
-})
+const schema = {
+    name: joi.string().min(6).required(),
+    email: joi.string().min(6).required().email(),
+    password: joi.string().min(6).required()
+};
 
-router.post("/login",(req,res)=>{
-    res.send("Login")
-})
-module.exports = router
+router.post('/register', async (req, res) => {
+    try {
+        // Validate the data before a a new user is created
+        const { error } = registerValidation(req.body)
+        if (error) return res.status(400).send(error.details[0].message);
+
+        const { name, email, password } = req.body;
+
+        // Check if a the user already exist 
+        const userexist = await User.findOne({email})
+        if(userexist) return res.status(400).send("User already exists")
+        // Create a new user
+        const user = new User({
+            name: name,
+            email: email,
+            password: password
+        });
+
+        const newUser = await user.save();
+        res.status(201).send(newUser);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.post("/login", (req, res) => {
+    res.send("Login");
+});
+
+module.exports = router;
